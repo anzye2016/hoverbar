70e7a20d6cec90b11b1d0f16d2f2e6827d773eb6
anzye2016 <anzye2016@163.com>
2026-07-26 22:43:47 +0800

    chore: simplify start.bat (remove duplicate pip install)

--- start.bat
+++ start.bat
@@ -3,7 +3,6 @@ cd /d "%~dp0"
 
 if not exist .venv (
     python -m venv .venv
-    .venv\Scripts\python -m pip install -r requirements.txt -q
 )
 
 .venv\Scripts\python -m pip install -r requirements.txt -q
