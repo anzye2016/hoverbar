6894c5935b2b4d8dcd7f2517f8182a93d85bf639
anzye2016 <anzye2016@163.com>
2026-07-26 22:29:33 +0800

    drop UAC elevation (no CPU temp benefit)

--- start.bat
+++ start.bat
@@ -1,13 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-fltmc >nul 2>&1 || (
-    powershell -Command "Start-Process cmd -ArgumentList '/c \"\"%~f0\"\"' -Verb RunAs"
-    exit /b
-)
-
-title HoverBar
-
 if not exist .venv (
     python -m venv .venv
     .venv\Scripts\python -m pip install -r requirements.txt -q
